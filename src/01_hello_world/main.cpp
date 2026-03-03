65c48d29426b25b10d494233643ff03c73d51b8a
Rushikesh Bodkhe <rushikeshbodkhe2@gmail.com>
2026-03-03 19:22:17 +0530

    book store

--- src/01_hello_world/main.cpp
+++ src/01_hello_world/main.cpp
@@ -2,5 +2,9 @@
 int main()
 {
     std::cout<< "Hello World !!" << std::endl;
+    // std::cout << "/*";
+    // std::cout << "*/";
+    // std::cout <</* "*/" */;
+    std::cout << /* "*/" /* "/*" */;
     return 0;
 }
